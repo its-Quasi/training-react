d5009ef832572cf07cd27aa44a0d349fde4763ec
Felipe Alferez Villamizar <83675214+its-Quasi@users.noreply.github.com>
2024-11-07 22:50:44 -0500

    feat: create custom form input

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,7 +1,31 @@
+import { Control, Controller } from "react-hook-form"
+
 interface Props {
+  name: string
   label: string
+  type?: string
+  control: Control<any>
+  errors: any
 }
 
-const CustomInput = ({ }: Props) => {
-
+const CustomInput = ({name, label, type, control, errors}: Props) => {
+  return (
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <Controller
+          name={name}
+          control={control}
+          render={
+            () => (
+              <input id={name}
+                type={type}
+                className={`form-control ${errors.name ? "is-invalid" : ""}`}
+              />
+            )
+          }
+        />
+      </div>
+  )
 }
+
+export default CustomInput
